6a2de55741c390ff8ce3b7d15358247074387d74
Andreas Hartmann <andreas.s.hartmann@fau.de>
2020-04-06 21:51:35 +0200

    Fixed string quotes in Runner

--- Runner.py
+++ Runner.py
@@ -7,4 +7,4 @@ timestamp = now.strftime("%m%d-%H%M%S")
 alpha = 1
 gamma = 0
 outpath =  "output/results_" + timestamp + "/"
-os.system('python3 main.py --OUTPATH ' + outpath + ' --Patch_Size 256 --Batch_Size 16 --LOSS focal_loss --Loss_Parms {"alpha": ' + str(alpha) + ', "gamma": ' + str(gamma) + '}')
+print('python3 main.py --OUTPATH ' + outpath + ' --Patch_Size 256 --Batch_Size 16 --LOSS focal_loss --Loss_Parms \"{\'alpha\': ' + str(alpha) + ', \'gamma\': ' + str(gamma) + '}\"')
