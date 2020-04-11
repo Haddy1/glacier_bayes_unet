00b49eae07c59c1ea3642e9c08f8a6fa67b80929
Andreas Hartmann <andreas.s.hartmann@fau.de>
2020-04-11 23:42:11 +0200

    made extra parameters for loss functions float values

--- helper_functions.py
+++ helper_functions.py
@@ -13,5 +13,5 @@ class StoreDictKeyPair(argparse.Action):
          my_dict = {}
          for kv in values.split(","):
              k,v = kv.split("=")
-             my_dict[k] = v
+             my_dict[k] = float(v)
          setattr(namespace, self.dest, my_dict)
