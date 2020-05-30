cf14ea7529b7398e10f6de3362e1017c39542853
Andreas Hartmann <andreas.s.hartmann@fau.de>
2020-05-30 17:20:02 +0200

    Forgot to add Bayed Dropout Layer

---
+++ layers/BayesDropout.py
@@ -0,0 +1,31 @@
+#import keras.backend as K
+from tensorflow.python.keras import backend as K
+from keras.layers import Layer, Dropout
+from tensorflow.python.keras.utils import tf_utils
+from tensorflow.python.ops import array_ops
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import nn
+
+
+class BayesDropout(Dropout):
+
+  def __init__(self, rate, noise_shape=None, seed=None, **kwargs):
+    super().__init__(rate, noise_shape=noise_shape, seed=seed, **kwargs)
+
+  def call(self, inputs, training=None):
+    if training is None:
+      training = K.learning_phase()
+
+    def dropped_inputs():
+      return nn.dropout(
+          inputs,
+          noise_shape=self._get_noise_shape(inputs),
+          seed=self.seed,
+          rate=self.rate)
+
+    output = tf_utils.smart_cond(training,
+                                 lambda: array_ops.identity(inputs),
+                                 dropped_inputs)
+
+    return output
+
